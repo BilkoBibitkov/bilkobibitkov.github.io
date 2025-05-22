ccb94d4d1380454b5164e1b86b1f5ba2d86752ec
Stanislav Georgiev <sgeorgiev@sgeorgi-ltmfj7m.internal.salesforce.com>
2025-05-22 08:51:26 -0700

    Add humorous mission statement to About page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -48,6 +48,40 @@ const About = () => {
           marginBottom: '20px',
           fontSize: '1.8em',
           fontWeight: '500'
+        }}>Our Mission</h2>
+        <p style={{
+          fontSize: '1.1em',
+          lineHeight: '1.6',
+          color: '#1d1d1f',
+          marginBottom: '20px'
+        }}>
+          This website is on a mission to make the world a better place, one metric at a time. We're not just building tools; 
+          we're creating a sanctuary for those who believe that 1000 meters should be called a kilometer, not 0.62 miles. 
+          Our goal is to provide a growing collection of web utilities that make life easier in this non-metric world, 
+          all while having a good laugh at the absurdity of imperial units.
+        </p>
+        <p style={{
+          fontSize: '1.1em',
+          lineHeight: '1.6',
+          color: '#1d1d1f'
+        }}>
+          As we evolve, you'll find more than just conversion tools. We'll be showcasing the beauty of the metric system, 
+          sharing humorous takes on measurement systems, and maybe even starting a support group for those traumatized 
+          by the imperial system. Because let's face it, who needs 12 inches in a foot when you can have 100 centimeters in a meter?
+        </p>
+      </section>
+
+      <section style={{ 
+        marginTop: '40px',
+        background: '#f5f5f7',
+        padding: '30px',
+        borderRadius: '12px'
+      }}>
+        <h2 style={{
+          color: '#1d1d1f',
+          marginBottom: '20px',
+          fontSize: '1.8em',
+          fontWeight: '500'
         }}>Contact</h2>
         <p style={{
           fontSize: '1.1em',
